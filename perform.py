439864203f250e4c85abaf923f8803de9f052238
eric <eric@hellman.net>
2017-04-02 13:15:44 -0400

    make it run faster

--- perform.py
+++ perform.py
@@ -6,7 +6,6 @@ with open('/Users/eric/github/local/clickstream_poetry/samples/fk.json') as fk:
     poem = json.load(fk)
 random.shuffle(poem['content'])
 for url in poem['content']:
-    requests.get(url)
+    requests.head(url, allow_redirects=False)
     print url
 print 'performed "{}". {}'.format(poem['title'], poem['copyright'])
-
